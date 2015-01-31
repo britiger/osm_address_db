a4daf44851cfc0d9fb8fab22b018bc774774055b
Christopher Lorenz <britiger@gmx.net>
2015-01-31 17:12:15 +0100

    missing fields

--- sql/fillMissingFields.sql
+++ sql/fillMissingFields.sql
@@ -1,10 +1,12 @@
+-- TODO: check performance with and without && operator
+-- TODO: Create Index on city_list  
 -- Postcode
 UPDATE import.osm_addresses AS addr
    SET "addr:postcode" = post.postal_code,
        "source:addr:postcode" = post.osm_id
 FROM (SELECT osm_id, postal_code, geometry FROM import.osm_postcode) AS post
 WHERE "addr:postcode" IS NULL
-  AND addr.geometry && post.geometry AND ST_WITHIN(addr.geometry, post.geometry);
+  AND ST_WITHIN(addr.geometry, post.geometry);
 
 -- Cityname
 UPDATE import.osm_addresses AS addr
@@ -12,4 +14,12 @@ UPDATE import.osm_addresses AS addr
        "source:addr:city" = city_osm_id
 FROM (SELECT city_osm_id, city_name, geometry FROM import.city_list) AS city
 WHERE "addr:city" IS NULL
-  AND addr.geometry && city.geometry AND ST_WITHIN(addr.geometry, city.geometry);
+  AND ST_WITHIN(addr.geometry, city.geometry);
+
+-- Country
+UPDATE import.osm_addresses AS addr
+   SET "addr:country" = "ISO3166-1",
+       "source:addr:country" = country.osm_id
+FROM (SELECT osm_id, "ISO3166-1", geometry FROM import.osm_admin_2) AS country
+WHERE "addr:country" IS NULL
+  AND ST_WITHIN(addr.geometry, country.geometry);
