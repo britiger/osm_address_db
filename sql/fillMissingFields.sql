fae8e95e1f90c7ebedddc51ec9a735915fab6871
Christopher Lorenz <britiger@gmx.net>
2015-02-01 20:17:58 +0100

    Adding Vacuum after updating osm_addresses

--- sql/fillMissingFields.sql
+++ sql/fillMissingFields.sql
@@ -1,5 +1,5 @@
 -- TODO: check performance with and without && operator
--- TODO: Create Index on city_list  
+
 -- Postcode
 UPDATE import.osm_addresses AS addr
    SET "addr:postcode" = post.postal_code,
@@ -7,6 +7,8 @@ UPDATE import.osm_addresses AS addr
 FROM (SELECT osm_id, postal_code, geometry FROM import.osm_postcode) AS post
 WHERE "addr:postcode" IS NULL
   AND ST_WITHIN(addr.geometry, post.geometry);
+
+VACUUM ANALYSE import.osm_addresses;
 
 -- Cityname
 UPDATE import.osm_addresses AS addr
@@ -16,6 +18,8 @@ FROM (SELECT city_osm_id, city_name, geometry FROM import.city_list) AS city
 WHERE "addr:city" IS NULL
   AND ST_WITHIN(addr.geometry, city.geometry);
 
+VACUUM ANALYSE import.osm_addresses;
+
 -- Country
 UPDATE import.osm_addresses AS addr
    SET "addr:country" = "ISO3166-1",
@@ -23,3 +27,5 @@ UPDATE import.osm_addresses AS addr
 FROM (SELECT osm_id, "ISO3166-1", geometry FROM import.osm_admin_2) AS country
 WHERE "addr:country" IS NULL
   AND ST_WITHIN(addr.geometry, country.geometry);
+
+VACUUM ANALYSE import.osm_addresses;
