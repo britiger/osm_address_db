6eca0da25e90bf754397e0a30c66d9f8b1bd7883
Christopher Lorenz <britiger@gmx.net>
2015-02-01 18:06:27 +0100

    adding vacuum after creating import

--- reimport.sh
+++ reimport.sh
@@ -11,13 +11,17 @@ export PGPASS=$password
 echo Creating import tables ...
 psql "dbname=$database host=$pghost user=$username password=$password port=5432" -f sql/createTables.sql > /dev/null
 
-# create Index for Initial import-schema
+# create Index for import-schema
 echo Creating indexes on import tables ...
 psql "dbname=$database host=$pghost user=$username password=$password port=5432" -f sql/createIndex.sql > /dev/null
 
-# create Views for Initial import-schema
+# create Views for import-schema
 echo Creating views on import tables ...
 psql "dbname=$database host=$pghost user=$username password=$password port=5432" -f sql/createView.sql > /dev/null
+
+# vacuum tables
+echo Vacuum on import tables ...
+psql "dbname=$database host=$pghost user=$username password=$password port=5432" -f sql/vacuumTables.sql > /dev/null
 
 # fill missing fields like postcode, city and country
 echo Fill postcode, city and country fields with surrounding polygons ...
---
+++ sql/vacuumTables.sql
@@ -0,0 +1,11 @@
+﻿-- Vacuum Tables in import Schema
+
+VACUUM ANALYSE import.osm_admin_2;
+VACUUM ANALYSE import.osm_admin_4;
+VACUUM ANALYSE import.osm_admin_6;
+VACUUM ANALYSE import.osm_admin_8;
+VACUUM ANALYSE import.osm_admin_9_up;
+VACUUM ANALYSE import.osm_places;
+VACUUM ANALYSE import.osm_postcode;
+VACUUM ANALYSE import.osm_roads;
+VACUUM ANALYSE import.osm_addresses;
