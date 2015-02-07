c70802332dbcb1ade2fbdf667db95b01a8a7d2de
Christopher Lorenz <britiger@gmx.net>
2015-02-07 22:27:13 +0100

    deleting(update) addr:street - entiees by associatedStreet

--- sql/deleteOldEntries.sql
+++ sql/deleteOldEntries.sql
@@ -43,3 +43,25 @@ WHERE postcode.osm_id=del.osm_id;
 DELETE FROM import.osm_roads AS roads
 USING delete_polygon_line AS del
 WHERE roads.osm_id=del.osm_id;
+
+-- delete dependencies in osm_addresses
+-- addr:country
+UPDATE import.osm_addresses AS addr
+SET "addr:country"=NULL, "source:addr:country"=NULL
+FROM delete_polygon as del
+WHERE "source:addr:country"=del.osm_id;
+-- addr:city
+UPDATE import.osm_addresses AS addr
+SET "addr:city"=NULL, "source:addr:city"=NULL
+FROM delete_polygon as del
+WHERE "source:addr:city"=del.osm_id;
+-- addr:postcode
+UPDATE import.osm_addresses AS addr
+SET "addr:postcode"=NULL, "source:addr:postcode"=NULL
+FROM delete_polygon as del
+WHERE "source:addr:postcode"=del.osm_id;
+-- addr:street
+UPDATE import.osm_addresses AS addr
+SET "addr:street"=NULL, "source:addr:street"=NULL
+FROM delete_rels as del
+WHERE "source:addr:street"=del.osm_id;
