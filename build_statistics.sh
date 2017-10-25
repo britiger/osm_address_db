eb012fb43694356773602e0b32f61ed68951de3f
Christopher Lorenz <britiger@gmx.net>
2017-10-25 21:12:43 +0200

    using echo_time in build_statistics.sh

--- build_statistics.sh
+++ build_statistics.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 . ./config
+. ./tools/bash_functions.sh
 
 # mapping user and password
 export PGHOST=$pghost
@@ -9,12 +10,17 @@ export PGUSER=$username
 export PGPASSWORD=$password
 export PGDATABASE=$database
 
+# define logfile name
+logfile_name=statistics_`date '+%Y-%m-%d_%H-%M-%S'`.log
+
 # Building statistics for current state
 
 # call sql for creating database
-echo Check schema statistics and create table if nessesary ...
+echo_time "Check schema statistics and create table if nessesary ..."
 psql -f sql/statsCreateTable.sql &> /dev/null
 
 # update current 
-echo Update statistics ...
+echo_time "Update statistics ..."
 psql -f sql/statsUpdateTable.sql > /dev/null
+
+echo_time "Statistics completed."
