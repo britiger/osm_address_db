2c4fae4193e4ed64904243e8b51ce947365ebc58
Christopher Lorenz <britiger@gmx.net>
2017-11-02 20:57:28 +0100

    Update roads and addresses only if needed

--- sql/statsUpdateTable.sql
+++ sql/statsUpdateTable.sql
@@ -19,17 +19,41 @@ VALUES (0,
 ON CONFLICT DO NOTHING;
 
 -- update all cities
-INSERT INTO statistics.road_addresses AS stats (osm_id, last_update, count_addresses, count_roads)
-SELECT oac.osm_id,
-(SELECT val FROM config_values WHERE key='update_ts_address')::timestamptz last_update,
-(SELECT count(*)
-    FROM
-    (SELECT 1
-    FROM import.osm_addresses addr
-    WHERE ST_INTERSECTS(addr.geometry,oa.geometry)
-    GROUP BY "addr:country", "addr:city", "addr:postcode", "addr:street", "addr:housename", "addr:housenumber", "addr:suburb") addr_list) count_addresses,
-(SELECT count(*) FROM import.city_roads WHERE city_osm_id=oac.osm_id) count_roads
-FROM import.osm_admin_city oac LEFT JOIN import.osm_admin oa ON oac.osm_id=oa.osm_id
-ON CONFLICT ON CONSTRAINT road_addresses_pk
-  DO UPDATE SET count_addresses=EXCLUDED.count_addresses, count_roads=EXCLUDED.count_roads
-  WHERE stats.count_addresses IS NULL OR stats.count_roads IS NULL;
+-- Update address_count
+DO $$
+BEGIN
+  IF (SELECT val FROM config_values WHERE key='update_ts_address')::timestamptz <= (SELECT max(last_update) FROM statistics.road_addresses WHERE count_addresses IS NOT NULL) THEN
+    RAISE NOTICE 'No update for addresses needed.';
+  ELSE
+    INSERT INTO statistics.road_addresses AS stats (osm_id, last_update, count_addresses)
+    SELECT oac.osm_id,
+    (SELECT val FROM config_values WHERE key='update_ts_address')::timestamptz last_update,
+    (SELECT count(*)
+        FROM
+        (SELECT 1
+        FROM import.osm_addresses addr
+        WHERE ST_INTERSECTS(addr.geometry,oa.geometry)
+        GROUP BY "addr:country", "addr:city", "addr:postcode", "addr:street", "addr:housename", "addr:housenumber", "addr:suburb") addr_list) count_addresses
+    FROM import.osm_admin_city oac LEFT JOIN import.osm_admin oa ON oac.osm_id=oa.osm_id
+    ON CONFLICT ON CONSTRAINT road_addresses_pk
+    DO UPDATE SET count_addresses=EXCLUDED.count_addresses
+    WHERE stats.count_addresses IS NULL;
+  END IF;
+END$$;
+
+-- Update road_count
+DO $$
+BEGIN
+  IF (SELECT val FROM config_values WHERE key='update_ts_full')::timestamptz <= (SELECT max(last_update) FROM statistics.road_addresses WHERE count_roads IS NOT NULL) THEN
+    RAISE NOTICE 'No update for roads needed.';
+  ELSE
+    INSERT INTO statistics.road_addresses AS stats (osm_id, last_update, count_roads)
+    SELECT oac.osm_id,
+    (SELECT val FROM config_values WHERE key='update_ts_full')::timestamptz last_update,
+    (SELECT count(*) FROM import.city_roads WHERE city_osm_id=oac.osm_id) count_roads
+    FROM import.osm_admin_city oac LEFT JOIN import.osm_admin oa ON oac.osm_id=oa.osm_id
+    ON CONFLICT ON CONSTRAINT road_addresses_pk
+    DO UPDATE SET count_roads=EXCLUDED.count_roads
+    WHERE stats.count_roads IS NULL;
+  END IF;
+END$$;
